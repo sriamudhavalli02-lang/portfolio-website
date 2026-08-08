1645d0a42062a576525eff3f8c3783200b24b58a
Sri Amudhavalli <sriamudhavalli02@gmail.com>
2026-08-08 17:22:28 +0530

    Deploy portfolio

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 require("./db");
 
@@ -12,13 +13,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..")));
 
 // =======================
 // Home Route
 // =======================
 
 app.get("/", (req, res) => {
-    res.send("Portfolio Backend Running...");
+    res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 // =======================
